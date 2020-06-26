e83047efd37723534e6fb9f9e32dab697bf924eb
Gaëtan SBAFFI <gaetansbaffi@live.fr>
2020-06-26 13:45:27 +0200

    added p5

--- p5/js/basket.js
+++
@@ -1,8 +0,0 @@
-function addToBasket() {
-	const num = Number(document.querySelector(".container").id);
-	fetch("https://oc-p5-api.herokuapp.com/api/teddies").then((response) =>
-		response.json().then((data) => {
-			localStorage.setItem(num, JSON.stringify(data[num]));
-		})
-	);
-}
